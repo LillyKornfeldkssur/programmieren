17fb4f49c9eb28dd1796bd22964eaade474ac07a
LillyKornfeldkssur <96227655+LillyKornfeldkssur@users.noreply.github.com>
2021-12-16 08:56:16 +0100

    Update Feet.py

--- Feet.py
+++ Feet.py
@@ -1,4 +1,3 @@
-input ("Wie viele cm möchtest du rechnen")
-x = 0.032808
-
-print(2*x)
+groesse = int(input(2Gib deine Groesse in cm an:"))
+fuss = groesse * 0.032808
+print("Deine Grésse in Fuss:" + str(fuss))
